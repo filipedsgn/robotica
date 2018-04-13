7368cea706789478eae6a73b42d848951e42c3b2
FilipeDSGN <filipe.dsgn@hotmail.com>
2018-04-13 14:56:36 -0300

    .

--- robotica.py
+++ robotica.py
@@ -1,5 +1,5 @@
 # Remover linha abaixo se não for executar no Jupyter Notebook
-%matplotlib notebook
+# %matplotlib notebook
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,50 +8,70 @@ from mpl_toolkits.mplot3d import axes3d
 print('Separar valores decimais com ponto')
 print('Sistema para rotação de eixos fixos XYZ, ou euler ZYX')
 
-dx = float(input('Deslocamento da origem do quadro B no eixo x: '))
-dy = float(input('Deslocamento da origem do quadro B no eixo y: '))
-dz = float(input('Deslocamento da origem do quadro B no eixo z: '))
-angX = np.radians(float(input('Rotação em X: ')))
-angY = np.radians(float(input('Rotação em Y: ')))
-angZ = np.radians(float(input('Rotação em Z: ')))
-xB = float(input('Posição Pb no eixo x: '))
-yB = float(input('Posição Pb no eixo y: '))
-zB = float(input('Posição Pb no eixo z: '))
+class Frame(object):
 
-r11 = np.cos(angZ) * np.cos(angY)
-r12 = (np.cos(angZ) * np.sin(angY) * np.sin(angX)) - (np.sin(angZ) * np.cos(angX))
-r13 = (np.cos(angZ) * np.sin(angY) * np.cos(angX)) + (np.sin(angZ) * np.sin(angX))
-r21 = np.sin(angZ) * np.cos(angY)
-r22 = (np.sin(angZ) * np.sin(angY) * np.sin(angX)) + (np.cos(angZ) * np.cos(angX))
-r23 = (np.sin(angZ) * np.sin(angY) * np.cos(angX)) - (np.cos(angZ) * np.sin(angX))
-r31 = -(np.sin(angY))
-r32 = np.cos(angY) * np.sin(angX)
-r33 = np.cos(angY) * np.cos(angX)
+    def __init__(self, dados):
 
-rot = np.matrix([[r11, r12, r13],
-                 [r21, r22, r23],
-                 [r31, r32, r33]])
+        global x, y, z, u, v, w
 
-pABorg = np.matrix([[dx], [dy], [dz]])
-pB = np.matrix([[xB], [yB], [zB]])
-pA = (rot * pB) + pABorg
+        self.angX, self.angY, self.angZ, self.dx, self.dy, self.dz, self.xB, self.yB, self.zB = dados
+        self.angX = np.radians(self.angX)
+        self.angY = np.radians(self.angY)
+        self.angZ = np.radians(self.angZ)
+        self.r11 = np.cos(self.angZ) * np.cos(self.angY)
+        self.r12 = (np.cos(self.angZ) * np.sin(self.angY) * np.sin(self.angX)) - (np.sin(self.angZ) * np.cos(self.angX))
+        self.r13 = (np.cos(self.angZ) * np.sin(self.angY) * np.cos(self.angX)) + (np.sin(self.angZ) * np.sin(self.angX))
+        self.r21 = np.sin(self.angZ) * np.cos(self.angY)
+        self.r22 = (np.sin(self.angZ) * np.sin(self.angY) * np.sin(self.angX)) + (np.cos(self.angZ) * np.cos(self.angX))
+        self.r23 = (np.sin(self.angZ) * np.sin(self.angY) * np.cos(self.angX)) - (np.cos(self.angZ) * np.sin(self.angX))
+        self.r31 = -(np.sin(self.angY))
+        self.r32 = np.cos(self.angY) * np.sin(self.angX)
+        self.r33 = np.cos(self.angY) * np.cos(self.angX)
 
-print('pB:\n{}\nrot:\n{}\npA:\n{}'.format(pB, rot, pA))
+        self.rot = np.matrix([[self.r11, self.r12, self.r13],
+                              [self.r21, self.r22, self.r23],
+                              [self.r31, self.r32, self.r33]])
 
-x = np.matrix([0, 0, 0, 0, dx, dx, dx, dx, 0])
-y = np.matrix([0, 0, 0, 0, dy, dy, dy, dy, 0])
-z = np.matrix([0, 0, 0, 0, dz, dz, dz, dz, 0])
+        self.aPborg = np.matrix([[self.dx], [self.dy], [self.dz]])
+        self.pB = np.matrix([[self.xB], [self.yB], [self.zB]])
+        self.pA = (self.rot * self.pB) + self.aPborg
 
-u = np.matrix([1, 0, 0, dx, rot[0,0], rot[0,1], rot[0,2], (rot * pB)[0,0], pA[0,0]])
-v = np.matrix([0, 1, 0, dy, rot[1,0], rot[1,1], rot[1,2], (rot * pB)[1,0], pA[1,0]])
-w = np.matrix([0, 0, 1, dz, rot[2,0], rot[2,1], rot[2,2], (rot * pB)[2,0], pA[2,0]])
+        x = np.append(x, [[self.dx, self.dx, self.dx, self.dx, 0, 0]], axis=1)
+        y = np.append(y, [[self.dy, self.dy, self.dy, self.dy, 0, 0]], axis=1)
+        z = np.append(z, [[self.dz, self.dz, self.dz, self.dz, 0, 0]], axis=1)
+        u = np.append(u, [[self.rot[0,0], self.rot[0,1], self.rot[0,2],
+                           (self.rot * self.pB)[0,0], self.pA[0,0], self.aPborg[0,0]]], axis=1)
+        v = np.append(v, [[self.rot[1,0], self.rot[1,1], self.rot[1,2],
+                           (self.rot * self.pB)[1,0], self.pA[1,0], self.aPborg[0,0]]], axis=1)
+        w = np.append(w, [[self.rot[2,0], self.rot[2,1], self.rot[2,2],
+                           (self.rot * self.pB)[2,0], self.pA[2,0], self.aPborg[0,0]]], axis=1)
+        self.info()
+
+    def info(self):
+        print('x({}°) y({}°) z({}°)'.format(np.degrees(self.angX), np.degrees(self.angY), np.degrees(self.angZ)))
+        print('dx({}), dy({}), dz({})'.format(self.dx, self.dy, self.dz))
+        print('Matriz de rotação:\n{}\n'.format(self.rot))
+        print('pB: {}'.format(self.pB.T))
+        print('pA: {}'.format(self.pA.T))
+
+x = np.matrix([0, 0, 0])
+y = np.matrix([0, 0, 0])
+z = np.matrix([0, 0, 0])
+u = np.matrix([1, 0, 0])
+v = np.matrix([0, 1, 0])
+w = np.matrix([0, 0, 1])
+
+frames = []
+quant = int(input('quantidade de quadros: '))
+
+for i in range(1, (quant + 1)):
+    print('Sistema{}, insira os dados separados por espaço: '.format(i))
+    print('AngX, AngY, AngZ, Trans. X, Trans. Y, Trans. Z, bP(x), bP(y), bP(z)')
+    frames.append(Frame(list(map(float, input().split()))))
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.quiver(x, y, z, u, v, w, arrow_length_ratio=0.1)
-ax.set_xlim(pA.min(), pA.max())
-ax.set_ylim(pA.min(), pA.max())
-ax.set_zlim(pA.min(), pA.max())
 ax.set_xlabel('eixo x')
 ax.set_ylabel('eixo y')
 ax.set_zlabel('eixo z')
